556f9edbdd7af6e0cfca7ed6c7e63ebef3af1b1a
smooto <melissasmoot@live.com>
2020-06-29 16:08:10 -0700

    Add route to create new post

--- __tests__/posts.test.js
+++ __tests__/posts.test.js
@@ -1,29 +1,33 @@
+require('dotenv').config();
+
 const chance = require('chance').Chance();
 const { agent, prepare, getLoggedInUser } = require('../lib/data-helpers/data-helpers');
 const Post = require('../lib/models/Post');
 
 describe('posts routes', () => {
-  // it('creates a post via POST', async() => {
-  //   const loggedInUser = await getLoggedInUser();
-  //   const newPost = {
-  //     photoUrl: chance.url(),
-  //     caption: chance.sentence(),
-  //     tags: ['spring', 'skateboarding', 'gaming', 'art']
-  //   };
+  it('creates a post via POST', async() => {
+    const user = prepare(await getLoggedInUser());
 
-  //   return agent
-  //     .post('/api/v1/posts')
-  //     .send(newPost)
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         _id: expect.anything(),
-  //         user: loggedInUser.id,
-  //         photoUrl: newPost.photoUrl,
-  //         caption: newPost.caption,
-  //         tags: newPost.tags
-  //       });
-  //     });
-  // });
+    const newPost = {
+      photoUrl: chance.url(),
+      caption: chance.sentence(),
+      tags: ['spring', 'skateboarding', 'gaming', 'art']
+    };
+
+    return agent
+      .post('/api/v1/posts')
+      .send(newPost)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          user: user._id,
+          photoUrl: newPost.photoUrl,
+          caption: newPost.caption,
+          tags: newPost.tags
+        });
+      });
+  });
+
   it('gets all posts', async() => {
     const posts = prepare(await Post.find());
 
@@ -33,6 +37,7 @@ describe('posts routes', () => {
         expect(res.body).toEqual(posts);
       });
   });
+
   it('gets a post by ID', async() => {
     const post = prepare(await Post.findOne());
 
@@ -40,6 +45,29 @@ describe('posts routes', () => {
       .get(`/api/v1/posts/${post._id}`)
       .then(res => {
         expect(res.body).toEqual(post);
+      });
+  });
+
+  it('updates a post via PATCH', async() => {
+    const user = prepare(await getLoggedInUser());
+
+    const newPost = {
+      photoUrl: chance.url(),
+      caption: chance.sentence(),
+      tags: ['spring', 'skateboarding', 'gaming', 'art']
+    };
+
+    return agent
+      .post('/api/v1/posts')
+      .send(newPost)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          user: user._id,
+          photoUrl: newPost.photoUrl,
+          caption: newPost.caption,
+          tags: newPost.tags
+        });
       });
   });
 });

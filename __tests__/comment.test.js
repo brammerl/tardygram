7b5c7d4f5bd64d1bf55234b62adb67cff29444c1
Max Lamb <maxlambcodes@gmail.com>
2020-06-29 14:20:00 -0700

    comment test and route written but not tested

--- __tests__/comment.test.js
+++ __tests__/comment.test.js
@@ -2,10 +2,33 @@ const { prepare } = require('../lib/data-helpers/data-helpers');
 
 const request = require('supertest');
 const app = require('../lib/app');
+const agent = request.agent(app);
 
-const Comment = require('../lib/models/Comment');
+// const Comment = require('../lib/models/Comment');
+const Post = require('../lib/models/Post');
+const User = require('../lib/models/User');
 
-describe('Comment routes', asunc() => {
+describe('Comment routes', async() => {
 
-  it
-})
+  it('Posts a comment', async() => {
+    const post = prepare(await Post.findOne());
+    const user = prepare(await User.findOne());
+
+    return agent
+      .post('/api/v1/comments')
+      .send({
+        commentBy: user.id,
+        post: post.id,
+        comment: 'Here are some words'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          commentBy: user.id,
+          post: post.id,
+          comment: 'Here are some words',
+          __v: 0,
+          id: expect.anything()
+        });
+      });
+  });
+});
